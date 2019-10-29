b8cc7e675af47dd61bd925f13efe805092fa0dcd
elywin <lukeeraelywin@gmail.com>
2019-10-29 16:16:52 +0300

    created user entry

--- supermarket.cpp
+++ supermarket.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main(){
+    string a,b;
+	cout<<"\t\t\tWelcome to UCUSupermarket!!\n\n";
+	cout<<"------------------------------------------------------------------------------\n\n";
+	cout<<"Enter name: ";
+	cin>>a;
+	cout<<"\n";
+	cout<<"Enter contact: ";
+	cin>>b;
+	cout<<"\n";
 
-    return 0;
+ return 0;
+
 }
